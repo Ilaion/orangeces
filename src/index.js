dab5215593a5b86cb5b5ab2692995b85ca59aabc
Ilaion <nyaftog@gmail.com>
2022-09-20 14:06:42 +0300

    browser router change on hashrouter index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, HashRouter} from "react-router-dom";
 import store from './redux/reduxStore';
 import ReactDOM from "react-dom/client";
 import {Provider} from "react-redux";
@@ -11,11 +11,11 @@ import {Provider} from "react-redux";
 const root = ReactDOM.createRoot(document.getElementById('root'));
     root.render(
         <React.StrictMode>
-            <BrowserRouter basename={process.env.PUBLIC_URL}>
+            <HashRouter >
                 <Provider store={store}>
                     <App />
                 </Provider>
-            </BrowserRouter>
+            </HashRouter>
         </React.StrictMode>
     );
 
